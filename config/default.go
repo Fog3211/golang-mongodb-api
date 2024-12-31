4b659c1c2bf3adfe10076ce30e867f2720542af8
Fog3211 <fog3211@163.com>
2024-12-31 14:29:17 +0800

    feat: add user model

--- config/default.go
+++ config/default.go
@@ -1,14 +1,24 @@
 package config
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
 // Config represents the application configuration structure
 type Config struct {
-	DBUri    string `mapstructure:"MONGODB_LOCAL_URI"`
-	RedisUri string `mapstructure:"REDIS_URL"`
-	Port     string `mapstructure:"PORT"`
+	DBUri                  string        `mapstructure:"MONGODB_LOCAL_URI"`
+	RedisUri               string        `mapstructure:"REDIS_URL"`
+	Port                   string        `mapstructure:"PORT"`
+	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
+	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
+	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY"`
+	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY"`
+	AccessTokenExpiresIn   time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRED_IN"`
+	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRED_IN"`
+	AccessTokenMaxAge      int           `mapstructure:"ACCESS_TOKEN_MAXAGE"`
+	RefreshTokenMaxAge     int           `mapstructure:"REFRESH_TOKEN_MAXAGE"`
 }
 
 // LoadConfig loads configuration from the specified path
